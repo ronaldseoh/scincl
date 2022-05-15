1ddd0c0e064ed3748d173e380993909effe684cb
Ronald Seoh <ronaldseoh@icloud.com>
2022-05-15 11:13:04 -0400

    Create get_scincl_triples.py

---
+++ get_scincl_triples.py
@@ -0,0 +1,9 @@
+import argparse
+
+from gdt.triples_miner import TriplesMinerArguments
+from gdt.triples_miner.generic import get_generic_triples
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
