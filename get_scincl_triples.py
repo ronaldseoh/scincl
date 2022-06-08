daa80fdbd94d80ac44e55c0780af524a3fac2de6
Ronald Seoh <ronaldseoh@icloud.com>
2022-06-08 18:54:13 +0900

    Update get_scincl_triples.py

--- get_scincl_triples.py
+++ get_scincl_triples.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--graph_limit')
     parser.add_argument('--graph_paper_ids_path')
     parser.add_argument('--graph_embeddings_path')
 
@@ -35,8 +34,9 @@ if __name__ == '__main__':
     with open(args.graph_paper_ids_path) as f:
         s2orc_paper_ids = json.load(f)  # S2ORC Ids
     
-    with open(args.train_s2orc_paper_ids_path) as f:
-        train_s2orc_paper_ids = json.load(f)
+    if args.train_s2orc_paper_ids_path is not None:
+        with open(args.train_s2orc_paper_ids_path) as f:
+            train_s2orc_paper_ids = json.load(f)
 
     with open(args.train_query_s2orc_paper_ids_path) as f:
         train_query_s2orc_paper_ids = json.load(f)
@@ -48,16 +48,8 @@ if __name__ == '__main__':
         do_normalize=False,  # normalize with ANN backend
         placeholder=triples_miner_args.ann_index_path is not None and os.path.exists(triples_miner_args.ann_index_path))
 
-    if args.graph_limit == 's2orc':
-        # Do not change input graph
-        # - None: no change
-        # - S2ORC: Utilize full citation graph of S2ORC without filtering
-        train_embeddings = graph_embeddings
-        train_s2orc_paper_id_to_idx = {pid: idx for idx, pid in enumerate(s2orc_paper_ids)}
-        train_idx_to_s2orc_paper_id = {idx: pid for idx, pid in enumerate(s2orc_paper_ids)}
-
-    elif args.graph_limit == 'specter':
-        # Limit citation graph to the papers that are also used by SPECTER
+    if args.train_s2orc_paper_ids_path is not None:
+        # Limit citation graph to the papers included in train_s2orc_paper_ids
 
         # Find indices in graph embeddings and extract vectors
         s2orc_paper_id_to_paper_idx = {pid: idx for idx, pid in enumerate(s2orc_paper_ids)}
@@ -77,6 +69,13 @@ if __name__ == '__main__':
 
         train_idx_to_s2orc_paper_id = {idx: pid for idx, pid in enumerate(train_s2orc_paper_ids)}
         train_s2orc_paper_id_to_idx = {pid: idx for idx, pid in enumerate(train_s2orc_paper_ids)}
+    else:
+        # Do not change input graph
+        # - None: no change
+        # - S2ORC: Utilize full citation graph of S2ORC without filtering
+        train_embeddings = graph_embeddings
+        train_s2orc_paper_id_to_idx = {pid: idx for idx, pid in enumerate(s2orc_paper_ids)}
+        train_idx_to_s2orc_paper_id = {idx: pid for idx, pid in enumerate(s2orc_paper_ids)}
 
     get_generic_triples(train_s2orc_paper_id_to_idx,
                         train_idx_to_s2orc_paper_id,
